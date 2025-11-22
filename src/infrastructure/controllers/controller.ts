3435967ad6f49fd7341c47bb1e3bc9840ab49122
Edem Veliev <edem010100@gmail.com>
2025-11-22 05:01:59 +0500

    add http request

--- src/infrastructure/controllers/controller.ts
+++ src/infrastructure/controllers/controller.ts
@@ -1,9 +1,84 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, HttpCode, HttpStatus, Inject, Post} from '@nestjs/common';
+import {Symbols} from "di/common";
+import {ProcessTransactionUsecase} from "usecases/process-transaction-usecase";
+import {ProcessTransactionDto} from "infrastructure/controllers/dtos/transactions/process-transaction-dto";
+import {getExceptionByError} from "infrastructure/controllers/exceptions/exceptions";
 
 @Controller('api')
 export class MainController {
   constructor(
-
+      @Inject(Symbols.usecases.transactions.process)
+      private readonly processTransactionUsecase: ProcessTransactionUsecase,
   ) {}
+
+    @HttpCode(HttpStatus.OK)
+    @Get('/settings')
+    public async getSettings(): Promise<void> {
+        // try {
+        //     const params = {
+        //         page: query.page,
+        //         limit: query.limit,
+        //         query: query.query,
+        //     };
+        //
+        //     const { users, page, limit, total } = await this.searchUsersUsecase.execute(params);
+        //
+        // } catch (error) {
+        //     throw getExceptionByError(error);
+        // }
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Get('/transactions')
+    public async getTransactions(): Promise<void> {
+        // try {
+        //     const params = {
+        //         page: query.page,
+        //         limit: query.limit,
+        //         query: query.query,
+        //     };
+        //
+        //     const { users, page, limit, total } = await this.searchUsersUsecase.execute(params);
+        //
+        // } catch (error) {
+        //     throw getExceptionByError(error);
+        // }
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Get('/transactions/:id')
+    public async getTransaction(): Promise<void> {
+        // try {
+        //     const params = {
+        //         page: query.page,
+        //         limit: query.limit,
+        //         query: query.query,
+        //     };
+        //
+        //     const { users, page, limit, total } = await this.searchUsersUsecase.execute(params);
+        //
+        // } catch (error) {
+        //     throw getExceptionByError(error);
+        // }
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('/transactions')
+    public async createTransaction(@Body() dto: ProcessTransactionDto): Promise<void> {
+        try {
+            const params = {
+                input: dto.inputText,
+                choices: dto.choices,
+                customQueries: dto.customQueries,
+                maskingMode: dto.maskingMode,
+                tailLength: dto.tailLength,
+            };
+
+            const result = await this.processTransactionUsecase.execute(params);
+
+        } catch (error) {
+            throw getExceptionByError(error);
+        }
+    }
 
 }
---
+++ src/infrastructure/controllers/dtos/transactions/process-transaction-dto.ts
@@ -0,0 +1,24 @@
+import { IsArray, IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
+import {RuleKind} from "domain/_processor/rules";
+import {MaskingMode} from "domain/_processor/processing-config";
+
+export class ProcessTransactionDto {
+    @IsString()
+    @MinLength(1)
+    inputText!: string;
+
+    @IsArray()
+    @IsString({ each: true })
+    choices!: RuleKind[];
+
+    @IsArray()
+    @IsString({ each: true })
+    customQueries!: string[];
+
+    @IsString()
+    @IsEnum(MaskingMode)
+    maskingMode!: MaskingMode;
+
+    @IsOptional()
+    tailLength?: number;
+}
